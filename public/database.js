c9dc9ac23d7d4bf4508e6e61f71bf72970ab740d
manueloff14 <manueloffcabrera@gmail.com>
2024-10-02 17:47:49 -0700

    cambios en la forma de conseguir los datos

--- public/database.js
+++ public/database.js
@@ -2,7 +2,7 @@ const db = [
     {
         "author": "Manuel Cabrera",
         "authorImage": "./img/foto_manuel_cabrera.jpeg",
-        "date": "11 September 2024, 10:30 p.m.",
+        "date": "11 Sep 2024, 10:30 p.m.",
         "postImage": "./img/mock-cumbre-inicio.png",
         "description": "I’m excited to introduce Cumbre.icu, a web search engine designed to help people in Colombia quickly and efficiently find job opportunities. This project is the culmination of many hours of hard work, aimed at making employment more accessible across the country. I hope you find it useful!",
         "likes": 132,
@@ -11,7 +11,7 @@ const db = [
     {
         "author": "Manuel Cabrera",
         "authorImage": "./img/foto_manuel_cabrera.jpeg",
-        "date": "11 September 2024, 10:30 p.m.",
+        "date": "11 Sep 2024, 10:30 p.m.",
         "postImage": "./img/mock-dyt-inicio.png",
         "description": "Welcome to our school project shoe store website! Built with Python and Flask and hosted on PythonAnywhere, this site is the result of our team's collaboration and effort. We aimed to create an intuitive and user-friendly platform where visitors can explore our selection of shoes. We hope you enjoy browsing through it as much as we enjoyed developing it!",
         "likes": 0,
@@ -20,7 +20,7 @@ const db = [
     {
         "author": "Manuel Cabrera",
         "authorImage": "./img/foto_manuel_cabrera.jpeg",
-        "date": "10 August 2024, 10:30 p.m.",
+        "date": "10 Aug 2024, 10:30 p.m.",
         "postImage": "./img/mock-cumbre-inicio.png",
         "description": "This is my portfolio, and I’m proud to finally share it with you all. It’s a reflection of my hard work, dedication, and passion for what I do. I hope you enjoy exploring it as much as I enjoyed creating it!",
         "likes": 0,
